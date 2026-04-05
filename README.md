a502e367f254b36c55659b3308558b135ca64136
haifengqiu <haifengqiu@gmail.com>
2026-04-05 22:33:30 +0800

    feat: 添加WezTerm基础配置及状态栏功能
    添加wezterm.lua主配置文件，引入keybindings和status模块
    实现状态栏显示时间、模式切换功能
    添加常用快捷键配置，包括窗口、标签页、窗格管理等
    更新README文档添加参考链接

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wezterm-config
 我的WezTerm配置. 尽量减少记忆. 尽量按需增加配置.
+
+## 参考
+- [官方配置-keyassignment](https://wezterm.org/config/lua/keyassignment/)
+- [lua结构及status](https://github.com/huahuaid/HUAHUANVIM/tree/wezterm)
+- [keybindings.lua](https://zhuanlan.zhihu.com/p/1973514411802108537)
---
+++ config/keybindings.lua
@@ -0,0 +1,43 @@
+return function(wezterm, config)
+  -- ========== 优先容易记 其次贴近浏览器行为 再次优先选用较近的ALT. 不常用的用ALT+Enter选择提示==========
+  config.keys = {
+    {
+        key = "Enter",
+        mods = "ALT",
+        action = wezterm.action.ShowLauncherArgs({
+            flags = "FUZZY|LAUNCH_MENU_ITEMS|DOMAINS|KEY_ASSIGNMENTS",
+        }),
+    },
+    -- ========== 窗口管理 ==========
+    { key = 'F11',      mods = 'NONE',  action = wezterm.action.ToggleFullScreen },
+    -- ========== 标签页管理 ==========
+    { key = 't',          mods = 'ALT',   action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+    { key = 'u',          mods = 'ALT',   action = wezterm.action.SpawnTab{DomainName = 'WSL:Ubuntu-22.04'} },
+    { key = 'm',          mods = 'ALT',   action = wezterm.action.ShowTabNavigator },
+    { key = 'w',          mods = 'ALT',   action = wezterm.action.CloseCurrentPane { confirm = false } },
+    { key = 'Tab',          mods = 'CTRL',   action = wezterm.action.ActivateTabRelative(1) },
+    { key = 'Tab',          mods = 'CTRL|SHIFT',   action = wezterm.action.ActivateTabRelative(-1) },
+    -- ========== 窗格分割 ==========
+    { key = '\\',          mods = 'ALT',   action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+    { key = '-',          mods = 'ALT',   action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+    -- ========== 窗格导航 (Leader + 方向键) ==========
+    { key = 'LeftArrow',          mods = 'ALT',   action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = 'DownArrow',          mods = 'ALT',   action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = 'UpArrow',          mods = 'ALT',   action = wezterm.action.ActivatePaneDirection("Up") },
+    { key = 'RightArrow',          mods = 'ALT',   action = wezterm.action.ActivatePaneDirection("Right") },
+    -- ========== 窗格大小调整 (ALT+Shift + 方向键) ==========
+    { key = 'LeftArrow',  mods = 'ALT|SHIFT', action = wezterm.action.AdjustPaneSize { "Left", 5 } },
+    { key = 'DownArrow',  mods = 'ALT|SHIFT', action = wezterm.action.AdjustPaneSize { "Down", 5 } },
+    { key = 'UpArrow',    mods = 'ALT|SHIFT', action = wezterm.action.AdjustPaneSize { "Up", 5 } },
+    { key = 'RightArrow', mods = 'ALT|SHIFT', action = wezterm.action.AdjustPaneSize { "Right", 5 } },
+    -- ========== 搜索与命令 ==========
+    { key = "f", mods = "CTRL", action = wezterm.action.Search("CurrentSelectionOrEmptyString") },
+    -- ========== 滚动 ==========
+    { key = "Home", mods = "SHIFT", action = wezterm.action.ScrollToTop },
+    { key = "End", mods = "SHIFT", action = wezterm.action.ScrollToBottom },
+    { key = 'PageUp', mods = 'SHIFT', action = wezterm.action.ScrollByPage(-1) },
+    { key = 'PageDown', mods = 'SHIFT', action = wezterm.action.ScrollByPage(1) },
+    { key = 'UpArrow', mods = 'SHIFT', action = wezterm.action.ScrollByLine(-1) },
+    { key = 'DownArrow', mods = 'SHIFT', action = wezterm.action.ScrollByLine(1) },
+  }
+end
