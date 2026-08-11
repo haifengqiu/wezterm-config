f8e3e7d61087632192928e30fdb82553f06ac9a0
administrator <administrator@users.ompt-team>
2026-08-11 16:50:09 +0800

    feat: 默认使用 PowerShell 并设置默认工作目录

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,10 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+-- 默认用 Windows PowerShell
+config.default_prog = { 'powershell.exe', '-NoLogo' }
+config.default_cwd = 'D:/workspace'
+
 -- require("config.appearance")(wezterm, config)
 require("config.keybindings")(wezterm, config)
 require("config.status")(wezterm, config)
